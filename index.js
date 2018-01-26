f23ab664cedfeeaacae7ebb50a8947132391a30d
Morgan Laupies <morgan.laupies@gmail.Com>
2018-01-26 12:28:14 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-export default () => {
+module.exports = function () {
   var sQtext = '[^\\x0d\\x22\\x5c\\x80-\\xff]';
   var sDtext = '[^\\x0d\\x5b-\\x5d\\x80-\\xff]';
   var sAtom = '[^\\x00-\\x20\\x22\\x28\\x29\\x2c\\x2e\\x3a-\\x3c\\x3e\\x40\\x5b-\\x5d\\x7f-\\xff]+';
